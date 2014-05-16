487ecb2b020f6eac723acdf68804fc4a734c31c5
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2014-05-16 11:45:09 +0100

    added RequireLogin to be used as authentication middleware

--- auth_helpers.go
+++ auth_helpers.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"fmt"
+	"github.com/go-martini/martini"
 	"github.com/jmoiron/sqlx"
 	"net/http"
 )
@@ -18,14 +19,14 @@ func FindUserFromId(req *http.Request, res http.ResponseWriter, db *sqlx.DB) (Us
 	} else {
 		userId, err := findUserIdInRequest(req)
 		if err != nil {
-			fmt.Println("user id not fount")
+			fmt.Println("user id not found")
 			fmt.Println(userId)
 			http.Error(res, "Not Authorized", http.StatusUnauthorized)
 			return user, err
 		}
 		getUserErr := db.Get(&user, "select id, account_id, is_account_admin from case_blocks_users where id=?", userId)
 		if getUserErr != nil {
-			fmt.Println("user not fount")
+			fmt.Println("user not found")
 			fmt.Println(userId)
 			fmt.Println(getUserErr)
 			http.Error(res, "Not Authorized", http.StatusUnauthorized)
@@ -33,4 +34,12 @@ func FindUserFromId(req *http.Request, res http.ResponseWriter, db *sqlx.DB) (Us
 		}
 	}
 	return user, nil
+}
+
+func RequireLogin(req *http.Request, res http.ResponseWriter, db *sqlx.DB, c martini.Context) {
+	if user, err := FindUserFromId(req, res, db); err == nil {
+		c.Map(user)
+	} else {
+		http.Redirect(res, req, "/users/sign_in", http.StatusFound)
+	}
 }
