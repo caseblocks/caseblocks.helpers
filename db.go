67900ed084cdb3548083ebbf99f7058759ab21b3
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2015-05-12 22:10:53 +0100

    Added NewMongoSession helper

--- db.go
+++ db.go
@@ -2,6 +2,8 @@ package helpers
 
 import (
 	"github.com/jmoiron/sqlx"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
 func NewSqlConnection(connectionString string, logger Log) *sqlx.DB {
@@ -13,4 +15,25 @@ func NewSqlConnection(connectionString string, logger Log) *sqlx.DB {
 	db, err := sqlx.Connect("mysql", connectionString)
 	PanicToLogIf(err, logger)
 	return db
+}
+
+func NewMongoSession(connectionString string, logger Log) *mgo.Session {
+	if logger == nil {
+		logger = NewConsoleLogger()
+	}
+	session, err := mgo.Dial(connectionString)
+	PanicToLogIf(err, logger)
+	return session
+}
+
+func ConvertStringToBSON(id string) bson.ObjectId {
+	return bson.ObjectIdHex(id)
+}
+
+func ConvertStringsToBSONs(ids []string) []bson.ObjectId {
+	results := make([]bson.ObjectId, len(ids))
+	for i, id := range ids {
+		results[i] = ConvertStringToBSON(id)
+	}
+	return results
 }
