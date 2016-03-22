e42824185ae8df9527c8fd86ffca875bf1f42a33
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2016-03-22 11:31:46 +0000

    upgraded to work with Rails 4.1 sessions

--- rails_session.go
+++ rails_session.go
@@ -2,54 +2,87 @@ package helpers
 
 import (
 	"errors"
-	"github.com/adeven/gorails/marshal"
-	"github.com/adeven/gorails/session"
 	"net/http"
+
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha1"
+	"encoding/base64"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"golang.org/x/crypto/pbkdf2"
 )
 
 const (
-	secret_key_base = "04ca87cb0ed96b33364b4a4aeace525a1a5d98b4e5852e32a2b3848b2191e334a0d776f19fd66ccbdab91f4aec45d037084d65e7afe9cf05e1a578e31a91d76f" // can be found in config/initializers/secret_token.rb
+	secret_key_base = "216daf722168dedd9ad0c0666e665e74cd08e55774237672b45c17b7f475efde5acdd19f769912cadeb14deae5e40dce8a037fbbbd76dc8d685d58f2cc75f97b" // can be found in config/initializers/secret_token.rb
 	salt            = "encrypted cookie"                                                                                                                 // default value for Rails 4 app
+	key_iter_num    = 1000
+	key_size        = 64
 )
 
-func getRailsSessionData(session_cookie string) (decrypted_cookie_data []byte, err error) {
-	decrypted_cookie_data, err = session.DecryptSignedCookie(session_cookie, secret_key_base, salt)
+func generateSecret(base, salt string) []byte {
+	return pbkdf2.Key([]byte(base), []byte(salt), key_iter_num, key_size, sha1.New)
+}
+
+func decodeCookieData(cookie []byte) (data, iv []byte, err error) {
+	vectors := strings.SplitN(string(cookie), "--", 2)
+
+	data, err = base64.StdEncoding.DecodeString(vectors[0])
+	if err != nil {
+		return
+	}
+
+	iv, err = base64.StdEncoding.DecodeString(vectors[1])
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func decryptCookie(cookie []byte, secret []byte) (dd []byte, err error) {
+	data, iv, err := decodeCookieData(cookie)
+
+	c, err := aes.NewCipher(secret[:32])
+	if err != nil {
+		return
+	}
+
+	cfb := cipher.NewCBCDecrypter(c, iv)
+	dd = make([]byte, len(data))
+	cfb.CryptBlocks(dd, data)
+
+	return
+}
+
+func DecryptSignedCookie(signed_cookie, secret_key_base, salt string) (session []byte, err error) {
+	cookie, err := url.QueryUnescape(signed_cookie)
+	if err != nil {
+		return
+	}
+
+	vectors := strings.SplitN(cookie, "--", 2)
+	data, err := base64.StdEncoding.DecodeString(vectors[0])
+	if err != nil {
+		return
+	}
+
+	session, err = decryptCookie(data, generateSecret(secret_key_base, salt))
+	if err != nil {
+		return
+	}
 
 	return
 }
 
 func getAuthUserId(decrypted_session_data []byte) (user_id int64, err error) {
-	unauthorized_user := errors.New("Unauthorized user")
-	invalid_auth_data := errors.New("Invalid auth data")
-
-	session_data, err := marshal.CreateMarshalledObject(decrypted_session_data).GetAsMap()
-	if err != nil {
-		return
-	}
-
-	warden_data, ok := session_data["warden.user.user.key"]
-	if !ok {
-		return 0, unauthorized_user
-	}
-
-	warden_user_key, err := warden_data.GetAsArray()
-	if err != nil {
-		return
-	}
-	if len(warden_user_key) < 1 {
-		return 0, invalid_auth_data
-	}
-
-	user_data, err := warden_user_key[0].GetAsArray()
-	if err != nil {
-		return
-	}
-	if len(user_data) < 1 {
-		return 0, invalid_auth_data
-	}
-
-	user_id, err = user_data[0].GetAsInteger()
-
+	userKeyRegex := regexp.MustCompile(`warden.user.user.key":\[\[(\d+)`)
+	matches := userKeyRegex.FindStringSubmatch(string(decrypted_session_data))
+	userIdInt, err := strconv.Atoi(matches[1])
+	user_id = int64(userIdInt)
 	return
 }
 
@@ -60,7 +93,8 @@ func findUserIdInRequest(req *http.Request) (int64, error) {
 	if err != nil {
 		return 0, errors.New("User cookie not found")
 	}
-	decrypted_session_data, err := getRailsSessionData(cookie.Value)
+
+	decrypted_session_data, err := DecryptSignedCookie(cookie.Value, secret_key_base, salt)
 	if err != nil {
 		return 0, errors.New("Unable to decrypt session data.")
 	}
