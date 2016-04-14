70aded005f1e6a6908a1ae232eaa244f5ac56973
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2016-04-14 01:36:29 +0100

    added account_repository

---
+++ account_repository.go
@@ -0,0 +1,45 @@
+package helpers
+
+import (
+	"github.com/emergeadapt/caseblocks.helpers/Godeps/_workspace/src/github.com/jmoiron/sqlx"
+)
+
+type AccountRepository interface {
+	FindById(id FKInt) (Account, error)
+	FindByAccountCode(code string) (Account, error)
+}
+
+func NewAccountRepository(db *sqlx.DB) AccountRepository {
+	return &relationalAccountRepository{db}
+}
+
+type relationalAccountRepository struct {
+	DB *sqlx.DB
+}
+
+func (r *relationalAccountRepository) FindById(id FKInt) (Account, error) {
+
+	var sql string
+	if r.DB.DriverName() == "postgres" {
+		sql = "select * from case_blocks_accounts where id=$1"
+	} else {
+		sql = "select * from case_blocks_accounts where id=?"
+	}
+
+	account := Account{}
+	err := r.DB.Get(&account, sql, id)
+	return account, err
+}
+
+func (r *relationalAccountRepository) FindByAccountCode(code string) (Account, error) {
+	var sql string
+	if r.DB.DriverName() == "postgres" {
+		sql = "select * from case_blocks_accounts where nickname=$1"
+	} else {
+		sql = "select * from case_blocks_accounts where nickname=?"
+	}
+
+	account := Account{}
+	err := r.DB.Get(&account, sql, code)
+	return account, err
+}
