559fc160ca61f599f926f8c6bd2771aceda81739
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2016-05-23 06:35:03 +0100

    removed Godep, expanded finders and the data columns retrieved

--- user_repository.go
+++ user_repository.go
@@ -1,7 +1,7 @@
 package helpers
 
 import (
-	"github.com/emergeadapt/caseblocks.helpers/Godeps/_workspace/src/github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx"
 )
 
 type UserRepository interface {
@@ -24,9 +24,9 @@ func (r *relationalUserRepository) FindById(id FKInt) (User, error) {
 
 	var sql string
 	if r.DB.DriverName() == "postgres" {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.id=$1"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.id=$1"
 	} else {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.id=?"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.id=?"
 	}
 
 	user := User{}
@@ -37,9 +37,9 @@ func (r *relationalUserRepository) FindById(id FKInt) (User, error) {
 func (r *relationalUserRepository) FindByAuthToken(token string) (User, error) {
 	var sql string
 	if r.DB.DriverName() == "postgres" {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.authentication_token=$1"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.authentication_token=$1"
 	} else {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.authentication_token=?"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.authentication_token=?"
 	}
 
 	user := User{}
@@ -50,9 +50,9 @@ func (r *relationalUserRepository) FindByAuthToken(token string) (User, error) {
 func (r *relationalUserRepository) FindByDisplayName(accountCode, name string) (User, error) {
 	var sql string
 	if r.DB.DriverName() == "postgres" {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.display_name=$1 and a.nickname=$2"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.display_name=$1 and a.nickname=$2"
 	} else {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.display_name=? and a.nickname=?"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.display_name=? and a.nickname=?"
 	}
 
 	user := User{}
@@ -63,9 +63,9 @@ func (r *relationalUserRepository) FindByDisplayName(accountCode, name string) (
 func (r *relationalUserRepository) FindByLogin(accountCode, login string) (User, error) {
 	var sql string
 	if r.DB.DriverName() == "postgres" {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.login=$1 and a.nickname=$2"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.login=$1 and a.nickname=$2"
 	} else {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.login=? and a.nickname=?"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.login=? and a.nickname=?"
 	}
 
 	user := User{}
@@ -76,9 +76,9 @@ func (r *relationalUserRepository) FindByLogin(accountCode, login string) (User,
 func (r *relationalUserRepository) FindByEmail(accountCode, email string) (User, error) {
 	var sql string
 	if r.DB.DriverName() == "postgres" {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.email=$1 and a.nickname=$2"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.email=$1 and a.nickname=$2"
 	} else {
-		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.email=? and a.nickname=?"
+		sql = "select u.id, u.account_id, u.is_account_admin, a.nickname, email, display_name, login, u.created_at, u.updated_at, u.authentication_token from case_blocks_users u, case_blocks_accounts a where u.account_id=a.id and u.email=? and a.nickname=?"
 	}
 
 	user := User{}
