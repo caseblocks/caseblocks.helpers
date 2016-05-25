c4b38beb108c2ff6ca0c21a72f4baebcc406bcb0
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2016-05-25 22:51:10 +0100

    secondary query to inject account code into case document

--- case_document_repository.go
+++ case_document_repository.go
@@ -9,16 +9,28 @@ type CaseDocumentRepository interface {
 	FindCaseDocumentById(caseId string) (CaseDocument, error)
 }
 
-func NewCaseDocumentRepository(mongoSession *mgo.Session) CaseDocumentRepository {
-	return &mongoCaseDocumentRepository{mongoSession}
+func NewCaseDocumentRepository(mongoSession *mgo.Session, accountRepo AccountRepository) CaseDocumentRepository {
+	return &mongoCaseDocumentRepository{mongoSession, accountRepo}
 }
 
 type mongoCaseDocumentRepository struct {
-	mongoSession *mgo.Session
+	mongoSession      *mgo.Session
+	accountRepository AccountRepository
 }
 
 func (r *mongoCaseDocumentRepository) FindCaseDocumentById(caseId string) (CaseDocument, error) {
 	var result CaseDocument
+
 	err := r.mongoSession.DB(FindMongoDbString()).C("case_blocks.case_documents").FindId(bson.ObjectIdHex(caseId)).One(&result)
-	return result, err
+	if err != nil {
+		return result, err
+	}
+
+	account, err := r.accountRepository.FindById(result.AccountId)
+	if err != nil {
+		return result, err
+	}
+	result.AccountCode = account.Nickname
+
+	return result, nil
 }
