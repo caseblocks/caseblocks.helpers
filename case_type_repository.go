198d680aad9f106471e952e6fd621fb3ea848465
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2016-05-25 22:49:58 +0100

    fixed SQL bug

--- case_type_repository.go
+++ case_type_repository.go
@@ -19,9 +19,9 @@ func (r *relationalCaseTypeRepository) FindCaseTypeById(accountCode string, case
 
 	var sql string
 	if r.DB.DriverName() == "postgres" {
-		sql = "select ct.id, ct.account_id, ct.name, ct.system_category from case_blocks_case_types ct, case_blocks_accounts a  where ct.account_id == a.id and ct.id = $1 and a.nickname = $2"
+		sql = "select ct.id, ct.account_id, ct.name, ct.system_category from case_blocks_case_types ct, case_blocks_accounts a  where ct.account_id = a.id and ct.id = $1 and a.nickname = $2"
 	} else {
-		sql = "select ct.id, ct.account_id, ct.name, ct.system_category from case_blocks_case_types ct, case_blocks_accounts a  where ct.account_id == a.id and ct.id = ? and a.nickname = ?"
+		sql = "select ct.id, ct.account_id, ct.name, ct.system_category from case_blocks_case_types ct, case_blocks_accounts a  where ct.account_id = a.id and ct.id = ? and a.nickname = ?"
 	}
 
 	err := r.DB.Get(&result, sql, caseTypeId, accountCode)
